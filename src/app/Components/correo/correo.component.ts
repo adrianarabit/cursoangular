10dd88b3de6a97e53ee0b81ff8eca6c42c098034
Adrián Arabit Ferre <adrian@adrianarabit.com>
2020-01-10 19:15:57 +0100

    Clase2

---
+++ src/app/Components/correo/correo.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-correo',
+  templateUrl: './correo.component.html',
+  styleUrls: ['./correo.component.scss']
+})
+export class CorreoComponent implements OnInit {
+  correo: any;
+
+  constructor() {
+    this.correo = {
+      titulo: "Titulo del Email",
+      cuerpo: "Cuerpo del Email, Cuerpo del Email, Cuerpo del Email, Cuerpo del Email",
+      emisor: "correoemisor@adrianarabit.com",
+      destinatario: "correodestinatario@adrianarabit.com"
+    }
+   }
+
+  ngOnInit() {
+  }
+
+}
